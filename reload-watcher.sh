aea98e4cc577a5d29ca13109d8af1c781680cb57
softnetix <46715846+softnetix@users.noreply.github.com>
2025-09-26 17:16:48 +0300

    Update reload-watcher.sh

--- reload-watcher.sh
+++ reload-watcher.sh
@@ -1,60 +1,15 @@
-#!/bin/bash
-set -e
+#!/bin/sh
 
-# How it works:
-# 1. Starts a background heartbeat loop that updates $HEARTBEAT_FILE every 5 seconds (for health checks)
-# 2. Uses inotifywait to watch recursively for "moved_to" events within the flag directory. Hidden/temporary files are ignored.
-# Other processes can atomically signal a reload by moving a file into this directory
-# 3. On every valid event, immediately runs "openresty -s reload"
+PORT=9999
 
-RELOAD_NEEDED_FLAG_DIR="/usr/local/openresty/nginx/conf/reload"
-mkdir -p "$RELOAD_NEEDED_FLAG_DIR"
+while true; do
+  nc -l -p $PORT -q 0 > /dev/null 2>&1 &
+  pid=$!
 
-SYNC_NEEDED_FILE="/usr/local/openresty/nginx/conf/sync/sync-needed"
-
-HEARTBEAT_FILE="/tmp/nginx-watcher.heartbeat"
-
-# daemon process for healthcheck
-(
-  while true; do
-    date +%s > "$HEARTBEAT_FILE"
-    sleep 5
-  done
-) &
-
-log() {
-    echo "$(date '+%Y/%m/%d %H:%M:%S') - [watcher] $1" >&2
-}
-
-reload_nginx() {
-    log "Reloading Nginx..."
-
+  (
+    head -c 1 <&3 >/dev/null 2>&1
     openresty -s reload
+  ) 3</proc/$pid/fd/0 &
 
-    sleep 8
-
-    if [ -e "$SYNC_NEEDED_FILE" ]; then
-        rm -f "$SYNC_NEEDED_FILE"
-        log "Removed: $SYNC_NEEDED_FILE"
-    else
-        log "File not found: $SYNC_NEEDED_FILE"
-    fi
-
-    log "Nginx reloaded successfully"
-}
-
-
-log "Watching $RELOAD_NEEDED_FLAG_DIR"
-
-# Watch for changes in RELOAD_NEEDED_FLAG_DIR
-while read -r FILE EVENT; do
-    BASENAME=$(basename "$FILE")
-    # Ignore hidden/temp files
-    if echo "$BASENAME" | grep -Eq '(^\.|\.tmp$|\.swp$)'; then
-        continue
-    fi
-
-    log "$EVENT: $FILE"
-
-    reload_nginx
-done < <(inotifywait -m -r -e moved_to --format '%w%f %e' "$RELOAD_NEEDED_FLAG_DIR")
+  wait $pid
+done
