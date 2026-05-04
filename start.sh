fea074b6fe6d957e4d801635761de0cc049303be
Anton Dubrouski <anton.dubrouski@softnetix.io>
2026-05-04 21:10:28 +0300

    services logs to stdout

--- start.sh
+++ start.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+set -e
+
+LOG_DIR=/usr/local/openresty/nginx/logs
+
+mkdir -p "$LOG_DIR"
+touch "$LOG_DIR/service__api.access.log" "$LOG_DIR/service__api.error.log"
+
+tail -n 0 -F "$LOG_DIR/service__api.access.log" &
+tail -n 0 -F "$LOG_DIR/service__api.error.log" >&2 &
 
 # run watcher in background
 /usr/local/openresty/reload-watcher.sh &
